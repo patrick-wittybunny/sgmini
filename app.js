bbe5624849595b7d13695f03b3df73c2c9bfe7aa
patrick-wittybunny <patrick.wittybunny@gmail.com>
2019-01-07 18:09:52 +0800

    Update

--- app.js
+++ app.js
@@ -44,6 +44,19 @@ app.use('/', express.static('/'), serveIndex('public/', {
     'icons': true
 }))
 
+
+var file = new static.Server('./public');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
+
 // Listen
 // app.listen(3000)
 app.listen(process.env.PORT);
+

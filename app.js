7ba2721ce43e5f3ff07823db627cefcb5e6b499d
patrick-wittybunny <patrick.wittybunny@gmail.com>
2019-01-07 18:17:01 +0800

    Update

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-// var static = require('node-static');
-// var file = new static.Server();
-// require('http').createServer(function (request, response) {
-//     request.addListener('end', function () {
-//         file.serve(request, response);
-//     }).resume();
-// }).listen(process.env.PORT || 3000);
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveIndex = require('serve-index')
+var serveStatic = require('serve-static')
 
-// var finalhandler = require('finalhandler')
-// var http = require('http')
-// var serveIndex = require('serve-index')
-// var serveStatic = require('serve-static')
+// Serve directory indexes for public/ftp folder (with icons)
+var index = serveIndex('public/', {
+    'icons': true
+})
 
-// // Serve directory indexes for public/ftp folder (with icons)
-// var index = serveIndex('/', {
-//     'icons': true
-// })
+// Serve up public/ftp folder files
+var serve = serveStatic('public/')
 
-// // Serve up public/ftp folder files
-// var serve = serveStatic('/')
+// Create server
+var server = http.createServer(function onRequest(req, res) {
+    var done = finalhandler(req, res)
+    serve(req, res, function onNext(err) {
+        if (err) return done(err)
+        index(req, res, done)
+    })
+})
 
-// // Create server
-// var server = http.createServer(function onRequest(req, res) {
-//     var done = finalhandler(req, res)
-//     serve(req, res, function onNext(err) {
-//         if (err) return done(err)
-//         index(req, res, done)
-//     })
-// })
-
-// // Listen
-// server.listen(3000)
+// Listen
+server.listen(process.env.port)
 
 
 // var express = require('express')
@@ -40,23 +32,14 @@
 // // Serve URLs like /ftp/thing as public/ftp/thing
 // // The express.static serves the file contents
 // // The serveIndex is this module serving the directory
-// app.use('/', express.static('/'), serveIndex('public/', {
+// app.use('/', express.static('/'), serveIndex('/public/', {
 //     'icons': true
 // }))
 
-var static = require('node-static');
-var file = new static.Server('./public');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT);
 
-// Listen
-// app.listen(3000)
+
+
+// // Listen
+// // app.listen(3000)
 // app.listen(process.env.PORT);
-

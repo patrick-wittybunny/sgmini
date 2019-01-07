d08077401cb663834b503219971708df8b88b00b
patrick-wittybunny <patrick.wittybunny@gmail.com>
2019-01-07 17:53:06 +0800

    Update

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT || 3000);
+// var static = require('node-static');
+// var file = new static.Server();
+// require('http').createServer(function (request, response) {
+//     request.addListener('end', function () {
+//         file.serve(request, response);
+//     }).resume();
+// }).listen(process.env.PORT || 3000);
+
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveIndex = require('serve-index')
+var serveStatic = require('serve-static')
+
+// Serve directory indexes for public/ftp folder (with icons)
+var index = serveIndex('/', {
+    'icons': true
+})
+
+// Serve up public/ftp folder files
+var serve = serveStatic('/')
+
+// Create server
+var server = http.createServer(function onRequest(req, res) {
+    var done = finalhandler(req, res)
+    serve(req, res, function onNext(err) {
+        if (err) return done(err)
+        index(req, res, done)
+    })
+})
+
+// Listen
+server.listen(3000)

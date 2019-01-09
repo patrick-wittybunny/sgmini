d4af5665ec1b3f06efbb62660cdef1030de360f3
kristiagail17 <kristia.wittybunny@gmail.com>
2019-01-09 12:46:21 +0800

    double tap

--- public/p5_clicker_kristia/sketch.js
+++ public/p5_clicker_kristia/sketch.js
@@ -14,6 +14,7 @@ var submitted = false;
 var percent = 0;
 var percentResult = 0;
 var successSound, failSound, pushSound;
+var released = true;
 
 function preload (){ 
   img = loadImage("assets/lava.png");  // Load the image
@@ -22,15 +23,19 @@ function preload (){
 function setup() {
   // img = loadImage("/try1/assets/lava.png");  // Load the image
 
-  createCanvas(600, 900);
+  createCanvas(window.innerWidth, window.innerHeight);
+  // createCanvas(600, 900);
 
   successSound = loadSound('assets/success.mp3');
   failSound = loadSound('assets/fail.mp3');
   pushSound = loadSound('assets/push.mp3');
 
   // button = createButton('click me');
-  // button.position(width * 0.45, height * 0.8);
+  // button.style('border-radius', '30%');
+  // button.style('font-size', '150px');
+  // button.position(width * 0.05, height * 0.7);
   // button.mousePressed(clickMe);
+  // button.doubleClicked(clickMe);
   // console.log(button);
 
   
@@ -71,10 +76,10 @@ function draw() {
   textSize(50);
 
   if(!started){
-  	text("CLICK TO START", width/2, height * 0.2);
+  	text("CLICK TO START", width/2, height * 0.1);
   }
-  text(timer + " sec", width/2, height * 0.35);
-  text(clicks + " clicks", width/2, height * 0.5);
+  text(timer + " sec", width/2, height * 0.25);
+  text(clicks + " clicks", width/2, height * 0.4);
   text("CLICK ME", width * 0.5, height * 0.8);
   
   // while (timer > 0) {  // this doesn't work because it's all happening at the same time
@@ -89,17 +94,17 @@ function draw() {
     	timer--;
     	if (timer == 0) {
 	    // text("GAME OVER", width/2, height*0.7);
-	   	createRestartButton();
+        createRestartButton();
 	}
   }
 
   if(started && timer == 0){
 		
 	if(clicks > 30){
-	  	text("Good Result", width/2, height * 0.2);
+	  	text("Good Result", width/2, height * 0.1);
 	}
 	else{
-	  	text("Bad Result", width/2, height * 0.2);
+	  	text("Bad Result", width/2, height * 0.1);
 	}
 
   }
@@ -119,33 +124,42 @@ function draw() {
 
 }
 
+function mouseReleased(){
+  released = true;
+  return false;
+}
+
 // When the user clicks the mouse
 function mousePressed() {
   // Check if mouse is inside the circle
+  if(!released){
+    return;
+  }
+  released = false;
   var d = dist(mouseX, mouseY, width * 0.5, height * 0.8);
   if (d < 100) {
 
-  	if(!started){
-  		started = true;
-	}
+    if(!started){
+    		started = true;
+  	}
 
-	if(started && timer > 0){
-	  	clicks++;
-	  	r = random(255);
-	    	g = random(255);
-	   	b = random(255);
+  	if(started && timer > 0){
+  	  	clicks++;
+  	  	r = random(255);
+  	    g = random(255);
+  	   	b = random(255);
 
-	   	pushSound.play();
-	}
+  	   	pushSound.play();
+  	}
 
   }
-
-
-
-  console.log("hello");
+  console.log("helloas");
 }
+// function doubleClicked() {
 
-function clickMe() {
+// }
+
+/*function clickMe() {
   // Check if mouse is inside the circle
   // var d = dist(mouseX, mouseY, 360, 200);
   // if (d < 100) {
@@ -162,12 +176,12 @@ function clickMe() {
   if(started && timer > 0){
   	clicks++;
   	r = random(255);
-    	g = random(255);
+    g = random(255);
    	b = random(255);
   }
 
   console.log("hello");
-}
+}*/
 
 function restart() {
 	started = false;
@@ -185,8 +199,10 @@ function createRestartButton() {
 
 	if(!restartButton){
 
-	    	restartButton = createButton('restart');
-	  	restartButton.position(width * 0.8, height * 0.8);
+	    restartButton = createButton('restart');
+      restartButton.style('font-size', '30px');
+	  	restartButton.position(width * 0.40, height * 0.48);
+      restartButton.touchStarted(restart); 
 	  	restartButton.mousePressed(restart); 
 	}
 	else{
